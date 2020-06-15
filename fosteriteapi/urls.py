17c002e640dc95f6a29e3c88e8c5376aab04c856
keithrpotempa <keithrpotempa@gmail.com>
2020-06-15 14:42:28 -0500

    login created. login and register tested with postman and working

--- fosteriteapi/urls.py
+++ fosteriteapi/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('register/', register_user),
-    # path('login/', login_user),
+    path('login/', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- fosteriteapp/views/user/__init__.py
+++ fosteriteapp/views/user/__init__.py
@@ -1,4 +1,4 @@
-# from .login import login_user
+from .login import login_user
 from .register import register_user
 from .users import Users
 from .user_serializer import UserSerializer
