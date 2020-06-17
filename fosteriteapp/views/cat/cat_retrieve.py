bde94b34589d5587e40655b6ebe45a2c97543cb6
keithrpotempa <keithrpotempa@gmail.com>
2020-06-17 12:34:48 -0500

    adjusted depth on cat_serializer to allow retrieving of adoption_status

--- fosteriteapp/views/cat/cat_retrieve.py
+++ fosteriteapp/views/cat/cat_retrieve.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from django.http import HttpResponseServerError
-from fosteriteapp.models import Cat
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework import status
 from .cat_serializer import CatSerializer
+from fosteriteapp.models import Cat
 
 def cat_retrieve(self, request, pk=None):
     """Handle GET requests for single cat
@@ -13,10 +14,8 @@ def cat_retrieve(self, request, pk=None):
     """
     try:
         cat = Cat.objects.get(pk=pk)
-
         serializer = CatSerializer(
             cat, many=False, context={'request': request})
         return Response(serializer.data)
     except Exception as ex:
-        print(ex)
         return HttpResponseServerError(ex)
